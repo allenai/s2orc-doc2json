261f859042295d77e5c5637dc3b31190ea948fdb
kyleclo <kyleclo@uw.edu>
2021-04-01 17:10:48 -0700

    SPP client; readme; clean up process PDF to use SPP client

---
+++ doc2json/spp2json/spp/README.md
@@ -0,0 +1,25 @@
+# python client for ScienceParsePlus 
+
+### setup
+
+Install [ScienceParsePlus](https://github.com/allenai/scienceparseplus). The README should document how to build and run the service via Docker.  The running service should be accessible at `http://localhost:8080`.
+
+
+### dependencies
+
+This assumes Python 3.7.
+
+### usage
+
+As a script, run:
+```
+python spp_client.py --input example.pdf --output example.json
+```
+
+As a Python library:
+```
+from doc2json.spp2json.spp.spp_client import SppClient
+
+client = SppClient()
+client.process('example.pdf', )
+```
--- doc2json/spp2json/spp/spp_client.py
+++ doc2json/spp2json/spp/spp_client.py
@@ -7,16 +7,22 @@ import glob
 import ntpath
 from typing import List
 
+import requests
 
 class SppClient:
-    def process(self, input: str, output: str):
-        raise NotImplementedError
+    def process(self, infile: str, outfile: str):
+        with open(infile, 'rb') as f_in:
+            files = {"pdf_file": (f_in.name, f_in, "multipart/form-data")}
+            r = requests.post('http://localhost:8080/detect', files=files)
+            layout = r.json()
+            with open(outfile, 'w') as f_out:
+                json.dump(layout, f_out, indent=4)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Client for ScienceParsePlus (SPP) services")
-    parser.add_argument("--input", default=None, help="path to the directory containing PDF to process")
-    parser.add_argument("--output", default=None, help="path to the directory where to put the results")
+    parser.add_argument("--input", default=None, help="path to the PDF to process")
+    parser.add_argument("--output", default=None, help="path to the target output file")
     args = parser.parse_args()
 
     input_path = args.input
