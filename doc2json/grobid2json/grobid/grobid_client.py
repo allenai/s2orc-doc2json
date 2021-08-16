8c62023a246ca49b33dbdafe67de0c5424974b67
lucylw <lucyw@allenai.org>
2021-08-16 15:53:25 -0700

    switch to localhost for a11y2 deployment

--- doc2json/grobid2json/grobid/grobid_client.py
+++ doc2json/grobid2json/grobid/grobid_client.py
@@ -18,7 +18,7 @@ require something scalable too, which is not implemented for the moment.
 '''
 
 DEFAULT_GROBID_CONFIG = {
-    "grobid_server": "172.16.0.128",
+    "grobid_server": "localhost",
     "grobid_port": "8070",
     "batch_size": 1000,
     "sleep_time": 5,
