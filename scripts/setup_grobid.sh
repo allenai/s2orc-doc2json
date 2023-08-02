a15089a72be7b07890d8398caa7ee535a80df2a0
Joe Hsu <joeh@allenai.org>
2023-08-02 14:11:36 -0700

    update deprecated config

--- scripts/setup_grobid.sh
+++ scripts/setup_grobid.sh
@@ -3,7 +3,7 @@
 # put in your doc2json directory here
 export DOC2JSON_HOME=$HOME/s2orc-doc2json
 
-# Download Grobid
+# # Download Grobid
 cd $HOME
 wget https://github.com/kermitt2/grobid/archive/0.7.3.zip
 unzip 0.7.3.zip
@@ -15,8 +15,7 @@ cd $HOME/grobid-0.7.3
 # increase max.connections to slightly more than number of processes
 # decrease logging level
 # this isn't necessary but is nice to have if you are processing lots of files
-cp $DOC2JSON_HOME/doc2json/grobid/config.yaml $HOME/grobid-0.7.3/grobid-service/config/config.yaml
-cp $DOC2JSON_HOME/doc2json/grobid/grobid.properties $HOME/grobid-0.7.3/grobid-home/config/grobid.properties
+cp $DOC2JSON_HOME/doc2json/grobid2json/grobid/grobid.yaml $HOME/grobid-0.7.3/grobid-home/config/grobid.yaml
 
 ## Start Grobid
 ./gradlew run
