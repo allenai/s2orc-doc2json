0cf0190b50bffd24116dd71eaf6481e40deffe18
Joe Hsu <joeh@allenai.org>
2023-08-02 14:12:13 -0700

    clean file

--- scripts/process_multiple_papers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Path to the directory containing PDF files 
-DIR=/net/nfs/s2-research/joeh/papers/
-
-# Loop through all PDF files and run command on each
-for f in "$DIR"/*.pdf; do
-  python doc2json/grobid2json/process_pdf.py -i "$f" -t temp_dir/ -o output_dir/ 
-done
